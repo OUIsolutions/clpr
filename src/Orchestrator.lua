e2fc2cf079c1736729e3d70dce5c5aec0a299e3b
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-16 20:14:54 -0300

    att

--- src/Orchestrator.lua
+++ src/Orchestrator.lua
@@ -2,6 +2,12 @@
 
 
 Orchestrator.newOrchestrator  = function (props)
+
+    if not props.args then 
+        error("args is required")
+    end 
+    
+
     local selfobject = heregitage.newMetaObject()
     selfobject.public_props_extend(props)
     return selfobject.public
