dbcae1124e7d9b6e71e10861dd9e980fb3f61d06
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-16 20:43:27 -0300

    att

--- src/Orchestrator.lua
+++ src/Orchestrator.lua
@@ -1,7 +1,7 @@
 
 
 
-MainModule.newOrchestrator  = function (props)
+function MainModule.newOrchestrator (props)
 
     if not props.args then 
         error("args is required")
@@ -12,3 +12,8 @@ MainModule.newOrchestrator  = function (props)
     selfobject.public_props_extends(props)
     return selfobject.public
 end
+
+function MainModule.internal_test()
+    local removed = Args.remove_start_str(string, "clpraction=teste", "clpraction=")
+    print(removed)
+end 
--- src/args.lua
+++ src/args.lua
@@ -2,7 +2,9 @@ function Args.starts_with(string_modlue, str, target_str)
     return string_modlue.sub(str, 1, #target_str) == target_str
 end
 
-
+function Args.remove_start_str(string_module, str, target_str)
+    return string_module.sub(str, #target_str + 1)
+end
 
 function Args.collect_entries(string_module, args)
     
