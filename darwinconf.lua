121d8cf2893c33b17ea37271608e73ba2656d442
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-16 20:52:16 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -1,17 +1,9 @@
-function install_dependencies()
-  os.execute("mkdir -p dependencies")
-  if not darwin.dtw.isfile("dependencies/herigitage.lua") then 
-    os.execute("curl -L https://github.com/OUIsolutions/LuaHeritage/releases/download/1.0.0/heregitage.lua -o dependencies/herigitage.lua")
-  end
-end
+
 
 function build_embed_code()
-local heregitage = darwin.dtw.load_file("dependencies/herigitage.lua")
 
   local all = {[[(function()]]}
     all[#all + 1] = darwin.dtw.load_file("objects.lua") .. "\n"
-    all[#all + 1] = "local Heregitage = (function()  "..heregitage .. " end\n)()\n"
-
     local files = darwin.dtw.list_files_recursively("src",true)
 
     for _, file in ipairs(files) do
