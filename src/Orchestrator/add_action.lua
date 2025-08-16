b0215a77c3bd63407650a3b9d0f3efa6f0030fc2
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-16 20:58:57 -0300

    att

--- src/Orchestrator/add_action.lua
+++ src/Orchestrator/add_action.lua
@@ -1,24 +1,22 @@
 
-function OrchestratorFactory.construct_add_action(selfobj)
-    selfobj.add_action = function (props)
-        if not props.name then
-            error("Action name is required")
-        end
-        if not props.callback then
-            error("Action callback is required")
-        end
-
-        for i=1,#selfobj.actions do
-            if selfobj.actions[i].name == props.name then
-                error("Action with name '" .. props.name .. "' already exists")
-            end
-        end
-        
-        local action = {
-            name = props.name,
-            callback = props.callback,
-        } 
-        selfobj.actions[#selfobj.actions + 1] = action
-        return props.name        
+function Orchestrator.add_action(public,private,props)
+    if not props.name then
+        error("Action name is required")
     end
+    if not props.callback then
+        error("Action callback is required")
+    end
+
+    for i=1,#public.actions do
+        if public.actions[i].name == props.name then
+            error("Action with name '" .. props.name .. "' already exists")
+        end
+    end
+    
+    local action = {
+        name = props.name,
+        callback = props.callback,
+    } 
+    public.actions[#public.actions + 1] = action
+    return props.name
 end
