4f8cf389bed726cb0efa4ecd0696a1a2f91ba9be
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-16 20:36:21 -0300

    att

--- src/args.lua
+++ src/args.lua
@@ -1,6 +1,9 @@
-
-function Args.starts_with(str,target_str)
+function Args.starts_with(string_modlue, str, target_str)
+    return string_modlue.sub(str, 1, #target_str) == target_str
 end
 
-function Args.collect_entries(args)
+
+
+function Args.collect_entries(string_module, args)
+    
 end
