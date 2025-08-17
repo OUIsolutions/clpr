293106ffbbca22419ac6b1cf8f9b48be736f2493
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-17 01:57:17 -0300

    att

--- src/Orchestrator/add_action.lua
+++ src/Orchestrator/add_action.lua
@@ -21,21 +21,25 @@ function PublicOrchestrator.add_action(public,private,props)
 
 
     if props.name == public.action_name then
+
+        local pid_path = public.dir.."/pid"
+        local pid = public.get_pid(pid_path)
+        public.write_file(pid_path, pid)
+
+
+        print("emulating wait start")
+        os.execute("sleep 1") -- wait for the action directory to be created
+        local started_path = public.dir.."/started"
+        public.write_file(started_path, "1")
+
+
         local entries_content_path = public.dir.."/entries.lua"
         local entries_content = public.load_file(entries_content_path)
         local callback_args = nil
         if entries_content then 
             callback_args = public.loader(entries_content)
         end
-        print("emulating wait start")
-        os.execute("sleep 1") -- wait for the action directory to be created
-        local started_path = public.dir.."/started"
-        public.write_file(started_path, "1")
-        print("action started")
-
-        local pid_path = public.dir.."/pid"
-        local pid = public.get_pid(pid_path)
-        public.write_file(pid_path, pid)
+ 
 
         local result = props.callback(callback_args)
         local result_serialized = public.dumper(result)
