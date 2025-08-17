02f59d9c586bd4470ed86830753678c2dbcce396
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-17 01:08:42 -0300

    att

--- src/Action/constructor.lua
+++ src/Action/constructor.lua
@@ -1,5 +1,5 @@
 
-function ActionConstructor.construct(public_orchestrator,action_name)
+function ActionConstructor.construct(public_orchestrator,action_name,args)
     local selfobject = heregitage.newMetaObject()
     selfobject.public_props_extends(public_orchestrator)
     local action_dir = public_orchestrator.database_path.."/"..public_orchestrator.get_pid() .."_"..public_orchestrator.total_runned_actions
@@ -7,7 +7,7 @@ function ActionConstructor.construct(public_orchestrator,action_name)
     sanitized_args[#sanitized_args+1] = "clpraction="..action_name
     sanitized_args[#sanitized_args+1] = "clprdir="..action_dir
     sanitized_args[#sanitized_args+1] = "&" 
-    local formmated_args = Args.format_args(public_orchestrator.string, sanitized_args)
-    print("Executing: clpr("..formmated_args..")")
+    local formmated_command = Args.format_args(public_orchestrator.string, sanitized_args)
+    
     return selfobject.public
 end 
--- src/Orchestrator/start.lua
+++ src/Orchestrator/start.lua
@@ -1,6 +1,18 @@
 
-function PublicOrchestrator.start_action(public,private,action_name)
+function PublicOrchestrator.start_action(public,private,action_name,args)
+   
+    local found = false
+    for i =1 ,#public.actions do
+        if public.actions[i].name == action_name then
+            found = true
+            break
+        end
+    end
+    if not found then 
+        error("Action '"..action_name.."' not found in orchestrator actions")
+    end
+   
     public.total_runned_actions = public.total_runned_actions + 1
-    return ActionConstructor.construct(public, action_name)
+    return ActionConstructor.construct(public, action_name,args)
 
 end 
